710af0275d14f79e81a1882dd9de7b2460a4cf51
Tyler Kellen <tyler@sleekcode.net>
2012-11-25 10:56:45 -0600

    add build-internal to default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,6 +112,6 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['clean', 'uglify', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
 
 };
