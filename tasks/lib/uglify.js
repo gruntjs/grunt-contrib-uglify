2a862a42b653bdc78a7d4969a32a641e4f0bcd20
Jmeas <jellyes2@gmail.com>
2014-01-16 23:08:04 -0500

    Fixes source file reference location
    Source files in the same will be referenced by `file.ext` instead of
    `/file.ext`

--- tasks/lib/uglify.js
+++ tasks/lib/uglify.js
@@ -42,8 +42,9 @@ exports.init = function(grunt) {
       var fileDir = path.dirname(file);
       var sourceMapDir = path.dirname(options.generatedSourceMapName);
       var relativePath = path.relative(sourceMapDir, fileDir);
+      var pathPrefix = relativePath ? (relativePath+path.sep) : "";
 
-      file = relativePath + path.sep + basename;
+      file = pathPrefix + basename;
 
       topLevel = UglifyJS.parse(code, {
         filename: file,
