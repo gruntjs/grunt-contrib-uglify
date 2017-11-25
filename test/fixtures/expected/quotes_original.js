602ced4b8e71fd9ae6cb74df58a649dc5f909256
alexlamsl <alexlamsl@gmail.com>
2017-11-26 06:29:46 +0800

    Update uglify-js to v3.2.0

--- test/fixtures/expected/quotes_original.js
+++ test/fixtures/expected/quotes_original.js
@@ -1,1 +1,1 @@
-function llama(){return["This is a single quote string","This is a double quote string",{this:"is part of an Object",and:'so is this',but:'this one has "mixed" quotes: \''}]}
+function llama(){return['This is a single quote string',"This is a double quote string",{this:"is part of an Object",and:'so is this',but:'this one has "mixed" quotes: \''}]}
