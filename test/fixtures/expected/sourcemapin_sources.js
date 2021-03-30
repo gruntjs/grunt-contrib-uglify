4e83e4520e6103d4a5e78dae2679699965a5de6a
Paul Schreiber <github.com@paulschreiber.com>
2021-03-30 05:26:31 -0400

    Update UglifyJS to 3.13.3 (#554)

--- test/fixtures/expected/sourcemapin_sources.js
+++ test/fixtures/expected/sourcemapin_sources.js
@@ -1,2 +1,2 @@
-(function(){var u,i,t;t=function(n){return n*n},u=[1,2,3,4,5],i={root:Math.sqrt,square:t,cube:function(n){return n*t(n)}},"undefined"!=typeof elvis&&null!=elvis&&alert("I knew it!"),function(n){for(var t,e=0,r=u.length;e<r;++e)t=u[e],n.push(i.cube(t));return n}.call(this,[])}).call(this);
+!function(){var t=function(n){return n*n},r=[1,2,3,4,5],u={root:Math.sqrt,square:t,cube:function(n){return n*t(n)}};"undefined"!=typeof elvis&&null!=elvis&&alert("I knew it!"),function(n){for(var t=0,e=r.length;t<e;++t)n.push(u.cube(r[t]));return n}.call(this,[])}.call(this);
 //# sourceMappingURL=sourcemapin_sources.js.map
