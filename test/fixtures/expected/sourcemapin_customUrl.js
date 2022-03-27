2cb31be2f4916f4e61ba3c44365596187d723ddd
Nacho García <hello@nachogarcia.dev>
2022-03-27 17:32:54 +0200

    Update uglify-js to v3.15.2 (#562)
    * Update uglify-js to v3.15.2
    
    Signed-off-by: Nacho García <hello@nachogarcia.dev>
    
    * update tests
    
    Co-authored-by: alexlamsl <alexlamsl@gmail.com>

--- test/fixtures/expected/sourcemapin_customUrl.js
+++ test/fixtures/expected/sourcemapin_customUrl.js
@@ -1,2 +1,2 @@
-!function(){var t=function(n){return n*n},r=[1,2,3,4,5],u={root:Math.sqrt,square:t,cube:function(n){return n*t(n)}};"undefined"!=typeof elvis&&null!=elvis&&alert("I knew it!"),function(n){for(var t=0,e=r.length;t<e;++t)n.push(u.cube(r[t]));return n}.call(this,[])}.call(this);
+!function(){var r,u,t;t=function(n){return n*n},r=[1,2,3,4,5],u={root:Math.sqrt,square:t,cube:function(n){return n*t(n)}},"undefined"!=typeof elvis&&null!=elvis&&alert("I knew it!"),function(n){for(var t=0,e=r.length;t<e;++t)n.push(u.cube(r[t]));return n}.call(this,[])}.call(this);
 //# sourceMappingURL=sourcemapin_customUrl.js.map
