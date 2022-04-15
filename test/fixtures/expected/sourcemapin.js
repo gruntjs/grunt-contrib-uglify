60785ae5d3b21c7a6fdd9c615c3e502f38c87b1e
alexlamsl <alexlamsl@gmail.com>
2022-04-16 03:16:16 +0800

    update dependencies - chalk@4.1.2 - grunt@1.5.2 - maxmin@3.0.0 - uglify-js@3.15.4

--- test/fixtures/expected/sourcemapin.js
+++ test/fixtures/expected/sourcemapin.js
@@ -1,3 +1,3 @@
 // Hello World
-!function(){var list,math,square;square=function(x){return x*x},list=[1,2,3,4,5],math={root:Math.sqrt,square:square,cube:function(x){return x*square(x)}},"undefined"!=typeof elvis&&null!=elvis&&alert("I knew it!"),function(accum$){for(var i$=0,length$=list.length;i$<length$;++i$)accum$.push(math.cube(list[i$]));return accum$}.call(this,[])}.call(this);
+!function(){var square=function(x){return x*x},list=[1,2,3,4,5],math={root:Math.sqrt,square:square,cube:function(x){return x*square(x)}};"undefined"!=typeof elvis&&null!=elvis&&alert("I knew it!"),function(accum$){for(var i$=0,length$=list.length;i$<length$;++i$)accum$.push(math.cube(list[i$]));return accum$}.call(this,[])}.call(this);
 //# sourceMappingURL=sourcemapin.js.map
